577d7b7ed0ca699fad946ae09503d0fa7bb5a167
Hitesh Shinde <130219369+hiteshshinde04@users.noreply.github.com>
2024-10-20 10:42:05 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,10 @@
+class AreaOfCircle
+{
+	public static void main(String[]args)
+{
+	double pi=3.14 , r=4;
+	double area;
+	area=pi*r*r;
+	System.out.println("Area of Circle="+(area));
+}
+}
---
+++ alptri.java
@@ -0,0 +1,17 @@
+class alptri
+{
+	public static void main(String[]args)
+	{
+	char i,j;
+	char c=1;
+		
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{	
+			System.out.print(c+" ");	
+		c++;
+		}
+	}
+	}
+}
